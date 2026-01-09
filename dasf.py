ec7f120cd2bf35324c49c7906286555de33ad9f5
ВÐakramtursunjanov0-stack <akramtursunjanov0@gmail.com>
2026-01-09 22:10:59 +0600

    commit

--- dasf.py
+++ dasf.py
@@ -2,7 +2,6 @@
 import flet as ft
 from datetime import datetime
 
-
 def main(page: ft.Page):
     page.title = "Мое первое приложение!"
     page.theme_mode = ft.ThemeMode.LIGHT
@@ -10,8 +9,32 @@ def main(page: ft.Page):
     text_hello = ft.Text("Hello world")
     name_input = ft.TextField(label="Введите имя", expand=True)
 
+    #
     greeting_history = []
     greeting_text = ft.Text("История приветствий:")
+
+    
+    def on_button_click(tet):
+        name = name_input.value.strip()
+        if name:
+            current_time = datetime.now()
+            text_hello.value = f"{current_time.strftime('%Y-%m-%d %H:%M:%S')} - Hello {name}"
+            text_hello.color = ft.Colors.GREEN
+
+            greeting_history.append({"name": name, "time": current_time})
+            update_history(greeting_history)
+        else:
+            text_hello.value = "Введите корректное имя"
+            text_hello.color = ft.Colors.RED
+
+        name_input.value = ""
+        page.update()
+
+    
+    def clear_history(tet):
+        greeting_history.clear()
+        greeting_text.value = "История приветствий:"
+        page.update()
 
     def update_history(history):
         if not history:
@@ -23,43 +46,26 @@ def main(page: ft.Page):
             ]
             greeting_text.value = "История приветствий:\n" + "\n".join(lines)
 
-    def on_button_click(e):
-        name = name_input.value.strip()
-        if name:
-            current_time = datetime.now()
-            text_hello.value = f"{current_time.strftime('%Y-%m-%d %H:%M:%S')} - Hello {name}"
-            text_hello.color = ft.Colors.GREEN
-
             greeting_history.append({"name": name, "time": current_time})
-            greeting_history[:] = greeting_history[-5:]  # 🔥 последние 5 приветствий
 
-            update_history(greeting_history)
-        else:
-            text_hello.value = "Введите корректное имя"
-            text_hello.color = ft.Colors.RED
 
-        name_input.value = ""
-        page.update()
-
-    def clear_history(e):
-        greeting_history.clear()
-        update_history(greeting_history)
-        page.update()
-
-    def filter_morning(e):
+    
+    def filter_morning(tet):
         morning = [item for item in greeting_history if item['time'].hour < 12]
         update_history(morning)
         page.update()
 
-    def filter_evening(e):
+    def filter_evening(tet):
         evening = [item for item in greeting_history if item['time'].hour >= 12]
         update_history(evening)
         page.update()
 
+    
+
     send_button = ft.ElevatedButton("SEND", icon=ft.Icons.SEND, on_click=on_button_click)
     clear_button = ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_history)
-    morning_button = ft.ElevatedButton("Утро (до 12:00)", on_click=filter_morning)
-    evening_button = ft.ElevatedButton("Вечер (после 12:00)", on_click=filter_evening)
+    morning_button = ft.ElevatedButton("Утро (до 12:00)", on_click=filter_morning, )
+    evening_button = ft.ElevatedButton("Вечер (после 12:00)", on_click=filter_evening,)
 
     name_input.on_submit = on_button_click
 
@@ -69,6 +75,5 @@ def main(page: ft.Page):
         ft.Row([morning_button, evening_button]),
         greeting_text
     )
-
 
 ft.app(target=main, view=ft.AppView.WEB_BROWSER)
